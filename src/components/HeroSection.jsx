00127b6609b2fbf3b992c1a2dbe94d6037056366
daniel-marek <danielmarek66@gmail.com>
2024-10-29 21:46:41 +0100

    Uprava hero sekce

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
-  )
+    <div className="flex flex-col items-center mt-6 lg:mt-20 space-y-10">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        VirtualR build tools 
+        <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
+          {" "}
+          for developers
+        </span>
+      </h1>
+      <p className="text-center text-lg text-neutral-500 max-w-4xl">
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam quis nulla. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero.
+      </p>
+    </div>
+  );
 }
 
 export default HeroSection
